f734f1da606273bf1cee819b37cb28e177771062
tofiqaskerov <tofiqaskerov71@gmail.com>
2022-04-15 15:49:36 +0400

    Conter

---
+++ Conter/Helper/gazhesablama.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conter.Helper
+{
+    public class gazhesablama
+    {
+        public void calc(decimal gaz, decimal limitQiymet, decimal illikLimit)
+        {
+            decimal netice;
+            Console.WriteLine("serfiyyati daxil edin: ");
+            var serfiyyat = Console.ReadLine();
+
+
+            if (Convert.ToDecimal(serfiyyat) > illikLimit)
+            {
+                decimal serfiyyatDec = Convert.ToDecimal(serfiyyat);
+                netice = (serfiyyatDec - illikLimit) * limitQiymet + gaz * illikLimit;
+                Console.WriteLine(netice);
+            }
+            else
+            {
+                Console.WriteLine("yanlisdir");
+            }
+        }
+
+
+    }
+}
---
+++ Conter/Program.cs
@@ -0,0 +1,101 @@
+﻿using Conter.Helper;
+decimal gaz = 0.22M;
+decimal limitQiymet = 0.25M;
+decimal illikLimit = 5000;
+
+
+//if (serfiyyat > illikLimit)
+//{
+//    netice = (serfiyyat - illikLimit) * limitQiymet + gaz + illikLimit;
+//    Console.WriteLine(netice);
+//}
+//else
+//{
+//    Console.WriteLine(gaz * serfiyyat);
+//}
+
+
+ 
+gazhesablama gazhesablama = new();
+gazhesablama.calc(gaz , limitQiymet, illikLimit);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
